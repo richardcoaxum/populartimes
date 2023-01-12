b1dc1c169e42c0010c902cdf5ba608db74bd520c
richardcoaxum <trey8coaxum@gmail.com>
2023-01-11 19:52:36 -0500

    Update app.py

--- populartimes/app.py
+++ populartimes/app.py
@@ -1,3 +1,3 @@
 import populartimes
-var = populartimes.get_id("AIzaSyDDJ3iPWWx-S6IU2A7j-jpeXZfYBBXEs6U", "ChIJmzXuUuO3t4kRr7FLUsl4K04")
+var = populartimes.get_id("AIzaSyDDJ3iPWWx-S6IU2A7j-jpeXZfYBBXEs6U", "ChIJ51cu8IcbXWARiRtXIothAS4")
 print(var)
